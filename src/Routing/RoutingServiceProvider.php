ed5ff40e84393b4c464157772b1c7d3e93d3a7c9
Adrian Calvet <adrian.calvet@smartbrand.es>
2018-05-22 23:54:48 +0200

    Add more helpers.

---
+++ src/Routing/RoutingServiceProvider.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace Ardiran\Core\Routing;
+
+use Themosis\Foundation\ServiceProvider;
+use Illuminate\Routing\Redirector;
+use Illuminate\Routing\UrlGenerator;
+
+class RoutingServiceProvider extends ServiceProvider {
+
+	/**
+	 * Add object of management of user agent
+	 *
+	 * @return void
+	 */
+	public function register(){
+
+		$this->app->bind('ardiran.urlgenerator', function ($container) {
+
+			$request = $container['request'];
+
+			$routes = $container['router']->getRoutes();
+
+			$routes->refreshNameLookups();
+
+			return new UrlGenerator($routes, $request);
+
+		});
+
+		$this->app->bind('ardiran.redirector', function($container){
+
+			return new Redirector($container['ardiran.urlgenerator']);
+
+		});
+
+	}
+
+}
--- src/helpers.php
+++ src/helpers.php
@@ -16,3 +16,58 @@ if ( ! function_exists( 'register_ajax_controllers' ) ) {
 	}
 
 }
+
+if (! function_exists('url')) {
+
+	/**
+	 * Generate a url for the application.
+	 *
+	 * @param  string  $path
+	 * @param  mixed   $parameters
+	 * @param  bool    $secure
+	 * @return \Illuminate\Contracts\Routing\UrlGenerator|string
+	 */
+	function url($path, $parameters = [], $secure = null){
+		return container('ardiran.urlgenerator')->to($path, $parameters, $secure);
+	}
+
+}
+
+if (! function_exists('secure_url')) {
+
+	/**
+	 * Generate a HTTPS url for the application.
+	 *
+	 * @param  string  $path
+	 * @param  mixed   $parameters
+	 * @return \Illuminate\Contracts\Routing\UrlGenerator|string
+	 */
+	function secure_url($path, $parameters = []){
+		return url($path, $parameters, true);
+	}
+
+}
+
+
+if (!function_exists('redirect')){
+
+	/**
+	 * Wp Redirect to route
+	 *
+	 * @param null $to
+	 * @param int $status
+	 * @param array $headers
+	 * @param null $secure
+	 * @return \Illuminate\Routing\Redirector|mixed
+	 */
+	function redirect($to = null, $status = 302, $headers = [], $secure = null){
+
+		if(is_null($to)){
+			return container('ardiran.redirector');
+		}
+
+		return container('ardiran.redirector')->to($to, $status, $headers, $secure);
+
+	}
+
+}
