a1587c5747b974dd61fe6cf4a8a348b9a4f0d87b
Adrian Calvet <adrian.calvet@smartbrand.es>
2018-06-06 16:24:16 +0200

    Add Wordpress base middlewares

---
+++ src/Core/Themosis/Routing/Middleware/Authenticate.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace Ardiran\Core\Themosis\Routing\Middleware;
+
+use Closure;
+
+/**
+ * Class Authenticate
+ * {@inheritdoc }
+ * @package Sb\Core\Themosis\Routing\Middleware
+ */
+class Authenticate{
+
+	/**
+	 * Handle an incomming request
+	 *
+	 * @param $request
+	 * @param Closure $next
+	 * @param null $guard
+	 */
+	public function handle($request, Closure $next, $guard = null){
+
+		if (!is_user_logged_in()) {
+			return \redirect('');
+		}
+
+		return $next($request);
+
+	}
+
+}
---
+++ src/Core/Themosis/Routing/Middleware/IfNotAuthenticate.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace Sb\Core\Themosis\Routing\Middleware;
+
+use Closure;
+
+/**
+ * Class IfNotAuthenticate
+ * {@inheritdoc }
+ * @package Sb\Core\Themosis\Routing\Middleware
+ */
+class IfNotAuthenticate{
+
+	/**
+	 * Handle an incomming request
+	 *
+	 * @param $request
+	 * @param Closure $next
+	 * @param null $guard
+	 */
+	public function handle($request, Closure $next, $guard = null){
+
+		if(is_user_logged_in()){
+			return \redirect('');
+		}
+
+		return $next($request);
+
+	}
+
+}
